18630ae5a14876a3a7701521d58fbb391bcbcb19
Oshin Acharya <oshin.acharya0664@gmail.com>
2024-11-26 22:02:26 -0600

    Updated methods and utils

--- src/Index.java
+++ src/Index.java
@@ -1,7 +1,66 @@
+
+import java.util.Scanner;
+
 import static utils.Methods.printOddOrEven;
+import static utils.Methods.printNaturalNumbersInReverse;
+import static utils.Methods.isPrime;
+import static utils.Methods.printPrimeNumbersUpToN;
+import static utils.Methods.sumOfPrimesUpToN;
+import static utils.Methods.isArmstrong;
+import static utils.Methods.printArmstrongNumbersUpToN;
+import static utils.Methods.isPerfect;
+import static utils.Methods.printPerfectNumbersUpToN;
 
 public class Index {
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
         printOddOrEven(false);
+        printNaturalNumbersInReverse();
+        int numberToCheck = 29;
+        if (isPrime(numberToCheck)) {
+            System.out.println(numberToCheck + " is a prime number.");
+        } else {
+            System.out.println(numberToCheck + " is not a prime number.");
+        }
+
+        numberToCheck = 50;
+        if (isPrime(numberToCheck)) {
+            System.out.println(numberToCheck + " is a prime number.");
+        } else {
+            System.out.println(numberToCheck + " is not a prime number.");
+        }
+        System.out.print("Enter a number (n) to find all prime numbers up to n: ");
+        int n = scanner.nextInt();
+
+        // Print all prime numbers up to n
+        printPrimeNumbersUpToN(n);
+
+        int sum = sumOfPrimesUpToN(n);
+        System.out.println("The sum of all prime numbers between 1 and " + n + " is: " + sum);
+
+        System.out.print("Enter a number to check if it's an Armstrong number: ");
+        int numberCheck = scanner.nextInt();
+        if (isArmstrong(numberCheck)) {
+            System.out.println(numberCheck + " is an Armstrong number.");
+        } else {
+            System.out.println(numberCheck + " is not an Armstrong number.");
+        }
+        System.out.print("Enter a number (n) to find all Armstrong numbers up to n: ");
+        int armstrongLimit = scanner.nextInt();
+        printArmstrongNumbersUpToN(armstrongLimit);
+
+        System.out.print("Enter a number to check if it's a Perfect number: ");
+        int perfectNumber = scanner.nextInt();
+        if (isPerfect(perfectNumber)) {
+            System.out.println(perfectNumber + " is a Perfect number.");
+        } else {
+            System.out.println(perfectNumber + " is not a Perfect number.");
+        }
+        System.out.print("Enter a number (n) to find all Perfect numbers up to n: ");
+        int perfectLimit = scanner.nextInt();
+        printPerfectNumbersUpToN(perfectLimit);
     }
 }
+
--- src/utils/Methods.java
+++ src/utils/Methods.java
@@ -1,10 +1,10 @@
 package utils;
 
 public class Methods {
-    public static void printOddOrEven (boolean isOdd){
+    public static void printOddOrEven(boolean isOdd) {
         System.out.println("Even numbers between 1 and 100 are:");
         for (int i = 1; i <= 100; i++) {
-            if (isOdd){
+            if (isOdd) {
                 if (i % 2 != 0) {
                     System.out.print(i + " ");
                 }
@@ -12,9 +12,95 @@ public class Methods {
                 if (i % 2 == 0) {
                     System.out.print(i + " ");
                 }
-
             }
-
         }
+        System.out.println();
+    }
+
+    public static void printNaturalNumbersInReverse() {
+        System.out.println("Natural numbers from 100 to 1 are:");
+        for (int i = 100; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // Divisible by a number other than 1 and itself
+            }
+        }
+        return true; // Prime number
+    }
+
+    public static void printPrimeNumbersUpToN(int n) {
+        System.out.println("Prime numbers between 1 and " + n + " are:");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int sumOfPrimesUpToN(int n) {
+        int sum = 0; // Initialize sum
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i; // Add the prime number to the sum
+            }
+        }
+        return sum; // Return the final sum
+    }
+
+    public static boolean isArmstrong(int number) {
+        int originalNumber = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length(); // Find the number of digits
+
+        // Calculate the sum of each digit raised to the power of digits
+        while (number > 0) {
+            int digit = number % 10;
+            sum += (int) Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        // Compare the sum to the original number
+        return sum == originalNumber;
+    }
+
+    public static void printArmstrongNumbersUpToN(int n) {
+        System.out.println("Armstrong numbers between 1 and " + n + " are:");
+        for (int i = 1; i <= n; i++) {
+            if (isArmstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static boolean isPerfect(int number) {
+        int sum = 0;
+
+        // Find all proper divisors and calculate their sum
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+    public static void printPerfectNumbersUpToN(int n) {
+        System.out.println("Perfect numbers between 1 and " + n + " are:");
+        for (int i = 1; i <= n; i++) {
+            if (isPerfect(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 }
